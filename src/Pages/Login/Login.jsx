8243bece2cbdb0e3fe25c46573face4396018ff0
Bao Trong <baotrongnh@gmail.com>
2024-09-27 13:58:55 +0700

    add: dependecies on package.json

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,12 +1,152 @@
+import { Icon } from '@iconify/react';
+import { Col, Row } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { TypeAnimation } from 'react-type-animation';
+import loginImage from '../../assets/Photos/background/login-img2.jpg';
 import './Login.scss';
 
 function Login() {
+     const [isValidate, setIsValidate] = useState(false);
+     const [formData, setFormData] = useState({ username: '', password: '' });
+     const [showPassword, setShowPassword] = useState(false)
+
+     const handleShowPassword = () => {
+          setShowPassword(prev => !prev);
+
+     }
+
+     const handleChangeInput = (e) => {
+          console.log(e.target.value)
+          setFormData({ ...formData, [e.target.name]: e.target.value })
+     }
+
+     useEffect(() => {
+          if (formData.username !== '' && formData.username !== '') {
+               setIsValidate(true)
+          } else {
+               setIsValidate(false)
+          }
+     }, [formData])
+
+     const handleLogin = (e) => {
+          e.preventDefault()
+          if (isValidate) {
+
+          } else {
+
+          }
+     }
+
+     const handleGoogleLogin = () => {
+          window.open('http://localhost:3000/auth/google', '_self');
+     }
+
+
+     useEffect(() => {
+          const params = new URLSearchParams(window.location.search);
+          const token = params.get('token');
+          if (token) {
+               localStorage.setItem('token', token);
+          }
+     }, []);
+
      return (
           <div className="login-page">
-               <h1>Test github</h1>
+               <div className="container">
+                    <Row align='center' className="login-block">
+                         <Col xs={0} md={12} className='side-information'>
+                              <img src={loginImage} alt="login-image" />
+                              <div className="animation-container">
+                                   <TypeAnimation
+                                        sequence={[
+                                             'Welcome to MentorMatch!',
+                                             2000,
+                                             'Connect with the best Mentor!',
+                                             2000,
+                                             'Log in to access your account...',
+                                             2000,
+                                             'MentorMatch: Your Career Partner!',
+                                             2000
+                                        ]}
+                                        wrapper="span"
+                                        speed={40}
+                                        repeat={Infinity}
+                                   />
+                              </div>
+                         </Col>
+                         <Col xs={24} md={12}>
+                              {/* <form className='login-form' onSubmit={handleLogin}> */}
+                              <form className='login-form' onSubmit={handleLogin} >
+                                   <h1 className='title'>Welcome Back!</h1>
+                                   <p className='welcome-content'>We're glad to see you back.</p>
+                                   <div className='input-block'>
+                                        <label htmlFor="username" className='label username'>Username <span id="input-username-error" style={{ color: 'red' }}></span></label>
+                                        <input
+                                             type="text"
+                                             className="input username"
+                                             id='username'
+                                             // value={formData.username}
+                                             name='username'
+                                             onChange={(e) => handleChangeInput(e)}
+
+                                        />
+                                   </div>
+
+                                   <div className='input-block'>
+                                        <label htmlFor="password" className='label password'>Password <span id="input-password-error" style={{ color: 'red' }}></span></label>
+                                        <input
+                                             // type={showPassword ? "text" : "password"}
+                                             className="input password"
+                                             id='password'
+                                             // value={formData.password}
+                                             name='password'
+                                             onChange={(e) => handleChangeInput(e)}
+                                             type={showPassword ? 'text' : 'password'}
+                                        />
+                                        {showPassword ? (
+                                             <Icon
+                                                  id='icon1'
+                                                  icon="mdi:eye"
+                                                  className='icon'
+                                                  onClick={handleShowPassword}
+                                                  title='Hide Password'
+                                             />
+                                        ) : (
+                                             <Icon
+                                                  id='icon2'
+                                                  icon="mdi:eye-off"
+                                                  className='icon'
+                                                  onClick={handleShowPassword}
+                                                  title='Show Password'
+                                             />
+                                        )}
+                                   </div>
+                                   <div id='info' style={{ color: 'red', textAlign: 'center', fontWeight: 700 }}></div>
+
+
+
+                                   <button className='button-login'>Log in</button>
+
+
+
+                                   <div id="info"></div>
+                                   <div className="line"></div>
+                                   <p className='text-or'>or</p>
+
+                                   <button className="button-google" onClick={handleGoogleLogin}>
+                                        <Icon className='icon-google' icon="logos:google-icon" />
+                                        <span>Log in with Google</span>
+                                   </button>
+                                   <p className='login-text'>
+                                        {/* <Link className='login-link' to='/signup'>Sign up</Link> */}
+                                        Don't have an account? <span className='login-link'>Sign up</span>
+                                   </p>
+                              </form>
+                         </Col>
+                    </Row>
+               </div>
           </div>
      );
 }
 
 export default Login;
-
