848f61417d36d3548911f9ba077d3f96d821bf10
Bao Trong <baotrongnh@gmail.com>
2024-10-11 00:40:39 +0700

    chore: fix bug on auth context

--- src/Routes/PrivateRoutesAuth.jsx
+++ src/Routes/PrivateRoutesAuth.jsx
@@ -1,15 +1,12 @@
 import { Navigate } from "react-router-dom";
 import PropTypes from 'prop-types';
 import { getToken } from "../utils/storageUtils";
-import { useContext } from "react";
-import { AuthContext } from "../Contexts/AuthContext";
 
 function PrivateRoutesAuth({ children }) {
      let isAuthenticated = true;
      const token = getToken();
-     const { currentUser } = useContext(AuthContext);
 
-     if (token === null && currentUser === null) {
+     if (!token) {
           isAuthenticated = false;
      }
 
