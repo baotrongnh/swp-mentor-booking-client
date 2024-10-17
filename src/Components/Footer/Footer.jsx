6f60b17ac79f331c473a6f38a04665e73c82af70
Aries <lehuy25032004@gmail.com>
2024-10-18 01:21:04 +0700

    feat: add footer ui

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,9 +1,54 @@
-import './Footer.scss'
+import "./Footer.scss";
+import { Link } from "react-router-dom";
+import { Icon } from '@iconify/react';
 
-export default function Footer() {
+function Footer() {
      return (
-          <footer className="footer">
-               <h1>FOOTER HERE</h1>
-          </footer>
-     )
+          <div className="footer">
+               <footer className="text-center">
+                    <div className="container">
+                         <section className="links">
+                              <div className="links-wrapper">
+                                   <div className="link-item">
+                                        <h6>
+                                             <Link to="/about">About Us</Link>
+                                        </h6>
+                                   </div>
+                                   <div className="link-item">
+                                        <h6>
+                                             <Link to="/help">Help</Link>
+                                        </h6>
+                                   </div>
+                                   <div className="link-item">
+                                        <h6>
+                                             <Link to="/contact">Contact</Link>
+                                        </h6>
+                                   </div>
+                              </div>
+                         </section>
+                         <hr className="divider" />
+                         <section className="content">
+                              <p>
+                                   Welcome to Mentor Booking System! We connect you with experienced mentors in various fields. Explore our platform to find the perfect mentor for your growth.
+                              </p>
+                         </section>
+                         <section className="social-icons">
+                              <Link to="" className="icon"><Icon icon="fe:facebook" /></Link>
+                              <Link to="" className="icon"><Icon icon="fa6-brands:square-twitter" /></Link>
+                              <Link to="" className="icon"><Icon icon="ri:google-fill" /></Link>
+                              <Link to="" className="icon"><Icon icon="mdi:instagram" /></Link>
+                              <Link to="" className="icon"><Icon icon="uil:linkedin" /></Link>
+                              <Link to="" className="icon"><Icon icon="uiw:github" /></Link>
+                         </section>
+                    </div>
+                    <div className="footer-bottom">
+                         <p>
+                              <small>2024 Mentor Booking System - All rights reserved</small>
+                         </p>
+                    </div>
+               </footer>
+          </div>
+     );
 }
+
+export default Footer;
--- src/Pages/StudentProfile/StudentProfile.jsx
+++ src/Pages/StudentProfile/StudentProfile.jsx
@@ -47,25 +47,18 @@ function StudentProfile() {
                             <h2>{currentUser.fullName || 'Please login to get information...'}</h2>
                             <p><strong>Email:</strong> {currentUser.email || ' abc@fpt.edu.vn'}</p>
                             <p><strong>Point:</strong> {currentUser.point || 0} {/*<Icon icon="ri:typhoon-fill" style={{ width: '2rem', height: '2rem' }}  />*/}</p>
-                            <Button className='edit-profile-btn' onClick={handleToggle} style={{ marginTop: '16px' }}>
+                            <Button className='edit-profile-btn custom-btn' onClick={handleToggle} style={{ marginTop: '16px' }}>
                                 <Icon icon="uil:edit" style={{ width: '1.8rem', height: '1.8rem' }} /> Edit Profile
+                            </Button>
+                            <Button className='become-mentor-btn custom-btn' type="primary" style={{ marginTop: '1.8rem', marginLeft: '2rem' }}>
+                                <Icon icon="ep:avatar" style={{ width: '1.8rem', height: '1.8rem' }} /> Become Mentor
                             </Button>
                         </Col>
                     </Row>
 
                     <EditProfile visible={isEditing} onClose={handleClose} />
 
-                    <div className="bio-information">
-                        <div className="top-bio-information">
-                            <h1 className='bio-h1'>Bio</h1>
-                            <Button className='edit-bio-btn'>
-                                <Icon icon="uil:edit" style={{ width: '1.8rem', height: '1.8rem' }} />Edit Bio
-                            </Button>
-                        </div>
-                        <p>
-                            Hi, My name is {currentUser.fullName}
-                        </p>
-                    </div>
+
                 </div>
             </div>
         </div>
