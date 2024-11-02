2f48902b22988a89a04d69d4ce44b5ccbe7e84cd
Bao Trong <nhbaotrong@gmail.com>
2024-11-02 13:09:49 +0700

    feat: add function reject apply mentor

--- src/Components/DescriptionEditor/DescriptionEditor.jsx
+++ src/Components/DescriptionEditor/DescriptionEditor.jsx
@@ -37,7 +37,7 @@ const DescriptionEditor = ({ defaultDescription, accountId }) => {
                     setIsEditing(false)
                }
           } else {
-               setIsEditing(true);
+               setIsEditing(true)
           }
      }
 
